75a0e3dffb7b0211fc5834d04957bcfbe810fc6f
KSAP02 <ksap.pradyumn@gmai.com>
2025-06-22 23:09:45 +0100

    Changed Local URLs to Render URLs for deployment

--- backend/main_backend.py
+++ backend/main_backend.py
@@ -22,8 +22,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Local URL
-    # allow_origins=["http://localhost:3000"], # deployment URL
+    # allow_origins=["http://localhost:3000"], # Local URL
+    allow_origins=["https://askmydoc-v3.onrender.com"], # deployment URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- src/app/api/parse_pdf/route.ts
+++ src/app/api/parse_pdf/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
+// const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000"; // Local Docker URL
+const BACKEND_URL = process.env.BACKEND_URL || "https://askmydoc-backend-v3.onrender.com"; // Render Deployment URL
 
 export async function POST(request: NextRequest) {
 	try {
